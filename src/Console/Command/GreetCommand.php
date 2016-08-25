bf06b88dd1ed7726243b39297ded0322250cab31
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-08-25 15:54:22 +0200

    TODO: GreetCommand (data object) + GreetCommandHandler

--- src/Console/Command/GreetCommand.php
+++ src/Console/Command/GreetCommand.php
@@ -14,6 +14,14 @@ class GreetCommand {
             $text = 'Hello';
         }
 
+        // TODO: just take arguments and options here,
+        // instanciate a GreetCommand data object and
+        // pass it on to a GreetCommandHandler
+        // see Michael Noback, Rock Tuss: CommandHandler
+        // advantage: use any cli framework, or even
+        // several in parallels, and / or web front controllers
+        // but just one class (GreetCommandHandler) for actually
+        // doing what's to be done
         $output->writeln($yell ? strtoupper($text) : $text);
     }
 
