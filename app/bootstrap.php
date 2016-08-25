085860ab02bd0482ab75cd9b77a213f24383e25f
Tim Quellmalz <tim.quellmalz@webentwickler.cc>
2016-08-26 01:18:51 +0200

    import function namespaces for DI\object(), DI\get()

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,15 @@
 <?php
 
+use function DI\object;
+use function DI\get;
+
 $params = [
     'fallbackName' => 'John'
 ];
 
 $services = [
-    Symfony\Component\Console\Output\OutputInterface::class => DI\object('Symfony\Component\Console\Output\ConsoleOutput'),
-    Friendsoft\Parser\NameParser::class => DI\object()->methodParameter('setFallbackName', 'fallbackName', DI\get('fallbackName'))
+    Symfony\Component\Console\Output\OutputInterface::class => object('Symfony\Component\Console\Output\ConsoleOutput'),
+    Friendsoft\Parser\NameParser::class => object()->methodParameter('setFallbackName', 'fallbackName', get('fallbackName'))
 ];
 
 return $services + $params;
